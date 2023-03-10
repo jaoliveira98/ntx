f6c2d1972bb7ebccf22e95d942708e3942fe089d
Joao Oliveira <jaoliveira98@gmail.com>
2023-03-10 09:36:30 +0000

    Movie Component

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
+
+import Movie from "./Movie";
 
 const Row = ({ title, fetchURL }) => {
   // Declare two state variables, "movies" and "like", using the useState hook.
   const [movies, setMovies] = useState([]);
-  const [like, setLike] = useState(false);
 
   // Use the useEffect hook to fetch movie data from the API when the component mounts.
   // The fetchURL parameter is included in the dependency array to ensure that the effect runs when fetchURL changes.
@@ -24,25 +24,7 @@ const Row = ({ title, fetchURL }) => {
       <div className="relative flex items-center">
         <div id={"slider"}>
           {movies.map((item, id) => (
-            <div className="w-[160px] sm:w-[200px] md:w-[280px] inline-block cursor-pointer relative p-2">
-              <img
-                className="w-full h-auto block"
-                src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
-                alt={item?.title}
-              />
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                <p className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-                  {item?.title}
-                </p>
-                <p>
-                  {like ? (
-                    <FaHeart className="absolute top-4 left-4 text-grey-300" />
-                  ) : (
-                    <FaRegHeart className="absolute top-4 left-4 text-grey-300" />
-                  )}
-                </p>
-              </div>
-            </div>
+            <Movie item={item} />
           ))}
         </div>
       </div>
