da24f843f94d75bde7f29b9bda27f863a49b410d
Joao Oliveira <jaoliveira98@gmail.com>
2023-03-13 17:34:22 +0000

    Main Component Improvements

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,27 +11,39 @@ const Main = () => {
 
   // Fetch popular movies from external API and update state
   useEffect(() => {
-    axios.get(requests.requestPopular).then((response) => {
-      setMovies(response.data.results);
-    });
+    const source = axios.CancelToken.source(); // Create a cancel token source
+
+    axios
+      .get(requests.requestPopular, {
+        cancelToken: source.token, // Set the cancel token to the request config
+      })
+      .then((response) => {
+        setMovies(response.data.results);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch popular movies: ", error);
+        setMovies([]); // Set a fallback value for movies state
+      });
+
+    return () => {
+      source.cancel("Request cancelled by user"); // Cancel the request when the component unmounts
+    };
   }, []);
 
   // Select a random movie from the list of movies in state
   const movie = movies[Math.floor(Math.random() * movies.length)];
 
-  // Extract relevant data from the selected movie
-  const title = movie?.title;
-  const backdropPath = movie?.backdrop_path;
-  const releaseDate = movie?.release_date;
-  const overview = movie?.overview;
+  // Extract relevant data from the selected movie using destructuring
+  const {
+    title,
+    backdrop_path: backdropPath,
+    release_date: releaseDate,
+    overview,
+  } = movie || {};
 
   // Truncate long strings for display purposes
   const truncateString = (str, num) => {
-    if (str?.length > num) {
-      return str.slice(0, num) + "...";
-    } else {
-      return str;
-    }
+    return str?.length > num ? `${str.slice(0, num)}...` : str;
   };
 
   return (
