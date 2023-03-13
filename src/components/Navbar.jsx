074b726e3082817bbdbd9d44ae9c1820b236597f
Joao Oliveira <jaoliveira98@gmail.com>
2023-03-13 17:26:03 +0000

    Adding Links to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { PrimaryBtn, DangerBtn } from "./buttons";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="z-[100] w-full absolute">
       <div className="container mx-auto flex items-center justify-between p-4 ">
-        <h1 className="text-red-600 text-4xl font-bold cursor-pointer uppercase">
-          Netflix
-        </h1>
+        <Link to="/">
+          <h1 className="text-red-600 text-4xl font-bold cursor-pointer uppercase">
+            Netflix
+          </h1>
+        </Link>
         <div className="flex gap-4">
-          <PrimaryBtn>Sign In</PrimaryBtn>
-          <DangerBtn>Sign up</DangerBtn>
+          <Link to="/login">
+            <PrimaryBtn>Sign In</PrimaryBtn>
+          </Link>
+          <Link to="/signup">
+            <DangerBtn>Sign up</DangerBtn>
+          </Link>
         </div>
       </div>
     </div>
