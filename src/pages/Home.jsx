2e4d805dabee8707ecb7803eb627488389883a4b
Joao Oliveira <jaoliveira98@gmail.com>
2023-03-13 14:16:02 +0000

    Home Component div removal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,13 @@ import requests from "../Requests";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <RandomMovieBanner />
       <Row id="1" title="UpComing" fetchURL={requests.requestUpcoming} />
       <Row id="2" title="Popular" fetchURL={requests.requestPopular} />
       <Row id="3" title="Trending" fetchURL={requests.requestTrending} />
       <Row id="4" title="Top Rated" fetchURL={requests.requestTopRated} />
-    </div>
+    </>
   );
 };
 
