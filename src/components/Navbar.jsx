fb12aa87df007dce2921dd84fa48c619b3312d70
Joao Oliveira <jaoliveira98@gmail.com>
2023-03-13 14:13:28 +0000

    Navbar Import Buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { PrimaryBtn, DangerBtn } from "./buttons";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
-      <h1 className="text-red-600 text-4xl font-bold cursor-pointer uppercase">
-        Netflix
-      </h1>
-      <div>
-        <button className="text-white pr-4">Sign In</button>
-        <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
-          Sign Up
-        </button>
+    <div className="z-[100] w-full absolute">
+      <div className="container mx-auto flex items-center justify-between p-4 ">
+        <h1 className="text-red-600 text-4xl font-bold cursor-pointer uppercase">
+          Netflix
+        </h1>
+        <div className="flex gap-4">
+          <PrimaryBtn>Sign In</PrimaryBtn>
+          <DangerBtn>Sign up</DangerBtn>
+        </div>
       </div>
     </div>
   );
