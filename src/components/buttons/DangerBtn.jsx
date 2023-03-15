dbe732780af200469bddd7cd4a6c26906e9a3c03
Joao Oliveira <jaoliveira98@gmail.com>
2023-03-15 10:55:30 +0000

    DangerBtn Update

--- src/components/buttons/DangerBtn.jsx
+++ src/components/buttons/DangerBtn.jsx
@@ -1,6 +1,7 @@
-const DangerBtn = ({ children, className }) => {
+const DangerBtn = ({ children, className, onClick }) => {
   return (
     <button
+      onClick={onClick}
       className={`bg-red-600 px-6 py-2 rounded cursor-pointer text-white ${className}`}
     >
       {children}
