489e1abba69e3934fae9d29267376398edf64852
Joao Oliveira <jaoliveira98@gmail.com>
2023-03-15 10:56:00 +0000

    Navbar Auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { PrimaryBtn, DangerBtn } from "./buttons";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="z-[100] w-full absolute">
       <div className="container mx-auto flex items-center justify-between p-4 ">
@@ -11,14 +24,23 @@ const Navbar = () => {
             Netflix
           </h1>
         </Link>
-        <div className="flex gap-4">
-          <Link to="/login">
-            <PrimaryBtn>Sign In</PrimaryBtn>
-          </Link>
-          <Link to="/signup">
-            <DangerBtn>Sign up</DangerBtn>
-          </Link>
-        </div>
+        {user?.email ? (
+          <div className="flex gap-4">
+            <Link to="/account">
+              <PrimaryBtn>Account</PrimaryBtn>
+            </Link>
+            <DangerBtn onClick={handleLogout}>Log out</DangerBtn>
+          </div>
+        ) : (
+          <div className="flex gap-4">
+            <Link to="/login">
+              <PrimaryBtn>Sign In</PrimaryBtn>
+            </Link>
+            <Link to="/signup">
+              <DangerBtn>Sign up</DangerBtn>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
