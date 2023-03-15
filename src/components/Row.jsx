e1476e22ab0835d65cf89fee1ef0480b7394cf26
Joao Oliveira <jaoliveira98@gmail.com>
2023-03-15 10:56:19 +0000

    Row Component Query implementation

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,60 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef } from "react";
 import axios from "axios";
+import { useQuery } from "react-query";
 import Movie from "./Movie";
 import { CiCircleChevLeft, CiCircleChevRight } from "react-icons/ci";
 
 const Row = ({ title, fetchURL, id }) => {
-  const [movies, setMovies] = useState([]);
-  const [error, setError] = useState(null);
+  const sliderRef = useRef(null);
 
-  useEffect(() => {
-    axios
-      .get(fetchURL)
-      .then((response) => {
-        setMovies(response.data.results);
-      })
-      .catch((error) => {
-        setError(error);
-      });
-  }, [fetchURL]);
+  const { data, error, isLoading } = useQuery(fetchURL, async () => {
+    const response = await axios.get(fetchURL);
+    return response.data.results;
+  });
 
   const slideLeft = () => {
-    let slider = document.getElementById("slider" + id);
-    slider.scrollLeft = slider.scrollLeft - 500;
+    sliderRef.current.scrollLeft -= 500;
   };
 
   const slideRight = () => {
-    let slider = document.getElementById("slider" + id);
-    slider.scrollLeft = slider.scrollLeft + 500;
+    sliderRef.current.scrollLeft += 500;
   };
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>Error: {error.message}</p>;
 
   return (
     <div className="container mx-auto my-4">
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      {error ? (
-        <div className="text-red-500">Error: {error.message}</div>
-      ) : (
-        <div className="relative flex items-center group">
-          <CiCircleChevLeft
-            onClick={slideLeft}
-            className="bg-white left-0 absolute rounded-full opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
-            size={30}
-          />
-          <div
-            id={"slider" + id}
-            className="w-full h-full snap-x overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
-          >
-            {movies.map((item, id) => (
-              <Movie key={id} item={item} />
-            ))}
-          </div>
-          <CiCircleChevRight
-            onClick={slideRight}
-            className="bg-white right-0 absolute rounded-full opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
-            size={30}
-          />
+      <div className="relative flex items-center group">
+        <CiCircleChevLeft
+          onClick={slideLeft}
+          className="bg-white left-0 absolute rounded-full opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={30}
+        />
+        <div
+          ref={sliderRef}
+          className="w-full h-full snap-x overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
+          {data.map((item) => (
+            <Movie key={item.id} item={item} />
+          ))}
         </div>
-      )}
+        <CiCircleChevRight
+          onClick={slideRight}
+          className="bg-white right-0 absolute rounded-full opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={30}
+        />
+      </div>
     </div>
   );
 };
